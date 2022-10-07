22cfd9857b092e5c26fccfaf6c4bc687261e20f7
Decta2 <tolqinov2003a@mail.ru>
2022-10-07 15:25:49 +0500

    my_basecamp 2

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ApplicationRecord
+    # after_destroy {notify admin of bad behavior}
+    has_many :comments, dependent: :destroy
+    validates :user_id, presence: true
+    has_one_attached :image
+    # has_many_attached :pictures
+    has_rich_text :body 
+    
+end
